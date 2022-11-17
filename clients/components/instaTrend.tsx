8eab31e3d41a7a47f4f4ebe6254e597cbacd0c7f
Balogun Razak <razak2free@gmail.com>
2022-11-17 16:47:25 +0100

    check the fetch i'm getting it worng somewhere

--- clients/components/instaTrend.tsx
+++ clients/components/instaTrend.tsx
@@ -1,16 +1,46 @@
 import style from '../styles/trend.module.css'
+import { Instadata } from './data/Data';
 
-const InstaTrend = () => {
+export  function InstaTrend() {
+ 
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <p>Instagram Trend <span>Worldwide</span></p>
+      <div className=  {style.header}>
+      <h2>Instagram Top <span>Trends</span></h2>
       </div>
+      {/* section2 */}
+      <div className={style.section2}>
       <div>
-        
+        <input type="date"placeholder='Today' style={{'border': 'none', 'fontSize': '26px'}}/>
+        {/* icon */}
       </div>
+      <div className={style.arrow}>
+        arrow
+      </div>
+      </div>
+
     </div>
   )
-}
+  }
 
-export default InstaTrend
+
+
+
+// (
+//   <div className={style.main}>
+//     <div className={style.header}>
+//       <p>Instagram Trend <span>Worldwide</span></p>
+//     </div>
+//     <div className='sectionTwo'>
+//       <div className={style.input}>
+//         <input  />
+//         <span>icon</span>
+//       </div>
+//       <div>arrow</div>
+//     </div>
+    
+//     <div>
+      
+//     </div>
+//   </div>
+// )
--- clients/pages/index.tsx
+++ clients/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import InstaTrend from "../components/instaTrend";
+import { InstaTrend } from "../components/instaTrend";
 import SideNav from "../components/sideNav";
 import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
@@ -8,13 +7,27 @@ import { useState } from "react";
 import TwitterTrend from "../components/twitterTrend";
 import TiktokTrend from "../components/tiktokTrend";
 import GoogleTrend from "../components/googleTrends";
+import { QueryCache, useQuery, QueryClientProvider, QueryClient} from 'react-query'
+import {ReactQueryDevtools} from 'react-query-devtools'
+import {GetServerSideProps} from 'next'
 
 interface PTypes {
   click:() => string;
   change: string;
+  info: any;
 }
 
-export default function Home() {
+// const queryClient = new QueryClient()
+
+// const toJSON = (_: Response) => _.json() 
+
+// const fetecher = () => fetch('https://jsonplaceholder.typicode.com/posts').then(toJSON);
+
+
+
+
+export default function Home( { data} ) {
+  console.log(data)
   const [change, setChange] = useState("1");
 
   const click: any = (e: any) => {
@@ -22,8 +35,14 @@ export default function Home() {
   };
   console.log(click.value)
 
+  // const {data: information, isLoading, error} =useQuery('information', fetecher)
+
+  // if(isLoading) return 'Loading...'
+  // if(error) return console.log(error)
+  // console.log(information)
 
   return (
+    // <QueryClientProvider client={queryClient} contextSharing={true}>
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -47,7 +66,22 @@ export default function Home() {
           )}
 
         </div>
+        {/* <ReactQueryDevtools /> */}
       </main>
     </div>
+    // </QueryClientProvider>
   );
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const data =  await res.json()
+  console.log(data)
+
+  return {
+    props: {
+      data
+    }
+  }
 }
