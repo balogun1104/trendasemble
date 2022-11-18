0fc3d254e90ca494be582112ba90ccdc62456afb
quadri salawudeen <olamiquadri1@gmail.com>
2022-11-18 12:27:59 +0100

    commit

--- clients/components/instaTrend.tsx
+++ clients/components/instaTrend.tsx
@@ -1,60 +1,33 @@
-import style from '../styles/trend.module.css'
-import data from '../components/data/Data';
+import style from "../styles/trend.module.css";
+import data from "../components/data/Data";
 
+const insta: any = Array.isArray(data());
+console.log(insta)
 
-const insta:any = () => data();
-
-export  function InstaTrend() {
- 
+export function InstaTrend() {
   return (
     <div className={style.main}>
-      <div className=  {style.header}>
-      <h2>Instagram Top <span>Trends</span></h2>
+      <div className={style.header}>
+        <h2>
+          Instagram Top <span>Trends</span>
+        </h2>
       </div>
       {/* section2 */}
       <div className={style.section2}>
-      <div>
-        <input type="date"placeholder='Today' style={{'border': 'none', 'fontSize': '26px'}}/>
-        {/* icon */}
-      </div>
-      <div className={style.arrow}>
-        arrow
-      </div>
-       
+        <div>
+          <input
+            type="date"
+            placeholder="Today"
+            style={{ border: "none", fontSize: "26px" }}
+          />
+          {/* icon */}
+        </div>
+        <div className={style.arrow}>arrow</div>
       </div>
 
-     {
-       insta.map((info: any) => {
-         return(
-              <div>
-                {info.name}
-              </div>
-         )
-       })
-     }
-
+      {insta.map((info: any) => {
+        return <div>{info.name}</div>;
+      })}
     </div>
-  )
-  }
-
-
-
-
-// (
-//   <div className={style.main}>
-//     <div className={style.header}>
-//       <p>Instagram Trend <span>Worldwide</span></p>
-//     </div>
-//     <div className='sectionTwo'>
-//       <div className={style.input}>
-//         <input  />
-//         <span>icon</span>
-//       </div>
-//       <div>arrow</div>
-//     </div>
-    
-//     <div>
-      
-//     </div>
-//   </div>
-// )
+  );
+}
